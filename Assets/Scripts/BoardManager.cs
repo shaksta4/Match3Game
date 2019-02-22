72737c36cec1d98afdae39d5bd7e7144f1b83cfe
Shakeel Khan <khanshak9@hotmail.com>
2019-02-23 01:35:58 +1300

    Neighbour finding
    Tiles can now id their neighbours. Working on selecting tile process

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,45 +8,108 @@ public class BoardManager : MonoBehaviour
     public int GridWidth;
     public int GridHeight;
     public GameObject tilePrefab;
+    public Tile PrevTile;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int y=0; y<GridHeight; y++)
+        CreateBoard();
+        FindNeighbours();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateBoard()
+    {
+        for (int y = 0; y < GridHeight; y++)
         {
-            for(int x=0; x < GridWidth; x++)
+            for (int x = 0; x < GridWidth; x++)
             {
                 GameObject g = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
                 g.transform.parent = gameObject.transform;
                 tiles.Add(g.GetComponent<Tile>());
 
                 tiles[GridWidth * y].isLeftEdge = true;
-                if(y == 0)
+                if (y == 0)
                 {
                     tiles[x].isBottomEdge = true;
                 }
             }
         }
         gameObject.transform.position = new Vector3(-2.5f, -2f, -0.5f);
+    }
 
-        for(int i=0;i<tiles.Count;i++)
+    void FindNeighbours()
+    {
+        for (int i = 0; i < tiles.Count; i++)
         {
-            if(!tiles[i].isLeftEdge)
+            if (!tiles[i].isLeftEdge)
             {
-                tiles[i].PreviousTile = tiles[i - 1];
+                tiles[i].LeftTile = tiles[i - 1];
             }
 
-            if(!tiles[i].isBottomEdge)
+            if (!tiles[i].isBottomEdge)
             {
                 tiles[i].BelowTile = tiles[i - GridWidth];
             }
         }
+        foreach (Tile t in tiles)
+        {
+            if(t.LeftTile != null)
+            {
+                t.AddNeighbour(t.LeftTile);
+                t.LeftTile.AddNeighbour(t);
+            }
 
+            if(t.BelowTile != null)
+            {
+                t.AddNeighbour(t.BelowTile);
+                t.BelowTile.AddNeighbour(t);
+            } 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TileSwap(Tile CurrentTile)
     {
-        
+        if (PrevTile == null)
+        {
+            print("First tile selected");
+            PrevTile = CurrentTile;
+            CurrentTile.ToggleSelect();
+        }
+        else if (PrevTile == CurrentTile)
+        {
+            print("Deselecting tile");
+            PrevTile = null;
+            CurrentTile.ToggleSelect();
+        }
+        //If Second clicked tile is neighbour with first clicked tile
+        else if (PrevTile.IsNeighbour(CurrentTile))
+        {
+            print("Second tile selected");
+            CurrentTile.ToggleSelect();
+        }
+        else
+        {
+            print("error");
+        }
+
+
+        /*else
+        {
+            if (PrevTile.IsNeighbour(CurrentTile))
+            {
+
+            }
+            else
+            {
+                PrevTile.ToggleSelect();
+                PrevTile = CurrentTile;
+            }
+        }*/
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public List<Tile> Neighbours = new List<Tile>();
-    public Sprite[] mySpriteImages = new Sprite[8];
-    public Tile PreviousTile;
+    public Sprite[] MySpriteImages = new Sprite[8];
+
+    public Tile LeftTile;
     public Tile BelowTile;
+    public Tile PrevTile;
+
     public bool isLeftEdge;
     public bool isBottomEdge;
+    public bool isSelected;
 
     private SpriteRenderer SpriteRenderVar;
-
 
     // Start is called before the first frame update
     void Start()
@@ -23,21 +26,47 @@ public class Tile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSelected)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
     }
 
     void CreateTile()
     {
-        int RandomTileSelector = Random.Range(0, mySpriteImages.Length);
+        int RandomTileSelector = Random.Range(0, MySpriteImages.Length);
 
         SpriteRenderVar = gameObject.GetComponent<SpriteRenderer>();
-        SpriteRenderVar.sprite = mySpriteImages[RandomTileSelector];
-        
+        SpriteRenderVar.sprite = MySpriteImages[RandomTileSelector];
     }
 
+    public void AddNeighbour(Tile t)
+    {
+        this.Neighbours.Add(t);
+    }
+
+    public bool IsNeighbour(Tile t)
+    {
+        if(Neighbours.Contains(t))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void ToggleSelect()
+    {
+        isSelected = !isSelected;
+    }
 
     void OnMouseDown()
     {
-        print("Clicked");
+
+        GameObject.Find("BoardManager").GetComponent<BoardManager>().TileSwap(this);
     }
 }
